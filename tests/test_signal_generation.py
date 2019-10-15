a15a9b81e58198321d7043fda0c36dac49382925
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2019-10-15 17:12:29 +0100

    Add early termination to greedy search.

--- tests/test_signal_generation.py
+++ tests/test_signal_generation.py
@@ -11,6 +11,8 @@ from vc2_conformance.picture_decoding import inverse_wavelet_transform
 
 from vc2_bit_widths.quantisation import forward_quant, inverse_quant
 
+from vc2_bit_widths.pyexp import Argument
+
 from vc2_bit_widths.linexp import (
     LinExp,
     strip_affine_errors,
@@ -691,144 +693,278 @@ def test_greedy_stochastic_search():
     assert np.all((new_input_picture >= input_min) & (new_input_picture <= input_max))
 
 
-def test_improve_synthesis_maximising_signal():
-    # This test will simply attempt to maximise a real test signal and verify
-    # that the procedure appears to produce an improved result.
+class TestImproveSynthesisMaximisingSignal(object):
     
-    # Create filter descriptions (of an arbtirary, smallish, asymmetric filter)
-    wavelet_index = WaveletFilters.le_gall_5_3
-    filter_params = LIFTING_FILTERS[wavelet_index]
-    dwt_depth = 1
-    dwt_depth_ho = 1
+    @pytest.fixture
+    def wavelet_index(self):
+        return WaveletFilters.le_gall_5_3
     
-    # Arbitrary choice of 10 bit signal range
-    input_min = -512
-    input_max = 511
+    @pytest.fixture
+    def filter_params(self, wavelet_index):
+        return LIFTING_FILTERS[wavelet_index]
     
-    max_quantisation_index = 63
-    quantisation_matrix = QUANTISATION_MATRICES[(
-        wavelet_index,
-        wavelet_index,
+    @pytest.fixture
+    def dwt_depth(self):
+        return 1
+    
+    @pytest.fixture
+    def dwt_depth_ho(self):
+        return 1
+    
+    @pytest.fixture
+    def quantisation_matrix(self, wavelet_index, dwt_depth, dwt_depth_ho):
+        return QUANTISATION_MATRICES[(
+            wavelet_index,
+            wavelet_index,
+            dwt_depth,
+            dwt_depth_ho,
+        )]
+    
+    @pytest.fixture
+    def analysis_input_linexp_array(self):
+        return SymbolArray(2)
+    
+    @pytest.fixture
+    def analysis_coeff_linexp_arrays(
+        self,
+        filter_params,
         dwt_depth,
         dwt_depth_ho,
-    )]
-    
-    analysis_input_linexp_array = SymbolArray(2)
-    analysis_coeff_linexp_arrays, _ = analysis_transform(
-        filter_params, filter_params,
-        dwt_depth, dwt_depth_ho,
         analysis_input_linexp_array,
-    )
+    ):
+        return analysis_transform(
+            filter_params, filter_params,
+            dwt_depth, dwt_depth_ho,
+            analysis_input_linexp_array,
+        )[0]
     
-    synthesis_input_linexp_arrays = make_symbol_coeff_arrays(dwt_depth, dwt_depth_ho)
-    synthesis_output_linexp_array, _ = synthesis_transform(
-        filter_params, filter_params,
-        dwt_depth, dwt_depth_ho,
+    @pytest.fixture
+    def synthesis_input_linexp_arrays(self, dwt_depth, dwt_depth_ho):
+        return make_symbol_coeff_arrays(dwt_depth, dwt_depth_ho)
+    
+    @pytest.fixture
+    def synthesis_output_linexp_array(
+        self,
+        filter_params,
+        dwt_depth,
+        dwt_depth_ho,
         synthesis_input_linexp_arrays,
-    )
+    ):
+        return synthesis_transform(
+            filter_params, filter_params,
+            dwt_depth, dwt_depth_ho,
+            synthesis_input_linexp_arrays,
+        )[0]
     
-    synthesis_input_pyexp_arrays = make_variable_coeff_arrays(dwt_depth, dwt_depth_ho)
-    synthesis_output_pyexp_array, _ = synthesis_transform(
-        filter_params, filter_params,
-        dwt_depth, dwt_depth_ho,
-        synthesis_input_pyexp_arrays,
-    )
+    @pytest.fixture
+    def synthesis_output_linexp_array(
+        self,
+        filter_params,
+        dwt_depth,
+        dwt_depth_ho,
+        synthesis_input_linexp_arrays,
+    ):
+        return synthesis_transform(
+            filter_params, filter_params,
+            dwt_depth, dwt_depth_ho,
+            synthesis_input_linexp_arrays,
+        )[0]
     
-    # Make an arbitrary choice of target to maximise
-    synthesis_target_linexp_array = synthesis_output_linexp_array
-    synthesis_target_pyexp_array = synthesis_output_pyexp_array
+    @pytest.fixture
+    def synthesis_transform_output(
+        self,
+        filter_params,
+        dwt_depth,
+        dwt_depth_ho,
+    ):
+        synthesis_input_pyexp_arrays = make_variable_coeff_arrays(dwt_depth, dwt_depth_ho)
+        return synthesis_transform(
+            filter_params, filter_params,
+            dwt_depth, dwt_depth_ho,
+            synthesis_input_pyexp_arrays,
+        )
     
-    # Run against all filter phases as some phases may happen to be maximised
-    # by the test signal anyway
-    num_improved_phases = 0
-    for tx in range(synthesis_target_linexp_array.period[0]):
-        for ty in range(synthesis_target_linexp_array.period[1]):
-            # Produce test signal
-            test_signal, new_tx, new_ty, mx, my, tmx, tmy = make_synthesis_maximising_signal(
-                analysis_input_linexp_array,
-                analysis_coeff_linexp_arrays,
-                synthesis_target_linexp_array,
-                synthesis_output_linexp_array,
-                tx, ty,
-            )
-            
-            synthesis_pyexp = synthesis_target_pyexp_array[new_tx, new_ty]
-            
-            kwargs = {
-                "h_filter_params": filter_params,
-                "v_filter_params": filter_params,
-                "dwt_depth": dwt_depth,
-                "dwt_depth_ho": dwt_depth_ho,
-                "quantisation_matrix": quantisation_matrix,
-                "synthesis_pyexp": synthesis_pyexp,
-                "test_signal": test_signal,
-                "input_min": input_min,
-                "input_max": input_max,
-                "max_quantisation_index": max_quantisation_index,
-                "random_state": np.random.RandomState(1),
-                "added_corruptions_per_iteration": (len(test_signal)+19)//20,  # 5%
-                "removed_corruptions_per_iteration": (len(test_signal)+8)//5,  # 20%
-                "added_iterations_per_improvement": 50,
-            }
-            
-            # Run without any greedy searches to get 'baseline' figure
-            base_test_signal, base_decoded_value, base_qi, base_total_iterations = improve_synthesis_maximising_signal(
-                number_of_searches=1,
-                base_iterations=0,
-                **kwargs
-            )
-            
-            # Run with greedy search to verify better result
-            imp_test_signal, imp_decoded_value, imp_qi, imp_total_iterations = improve_synthesis_maximising_signal(
-                number_of_searches=3,
-                base_iterations=100,
-                **kwargs
-            )
-            assert imp_total_iterations >= 3 * 100
-            
-            # Ensure new test signal is normalised to polarities
-            for value in imp_test_signal.values():
-                assert value in (-1, +1)
-            
-            # Should have improved over the test pattern alone
-            if abs(imp_decoded_value) > abs(base_decoded_value):
-                num_improved_phases += 1
-            
-            # Check to see if decoded value matches what the pseudocode decoder
-            # would produce
-            xs, ys = zip(*imp_test_signal)
-            width = max(xs) + 1
-            height = max(ys) + 1
-            imp_test_signal_picture = [
-                [
-                    input_min
-                    if imp_test_signal.get((x, y), 0) < 0 else
-                    input_max
-                    for x in range(width)
+    @pytest.fixture
+    def synthesis_output_pyexp_array(self, synthesis_transform_output):
+        return synthesis_transform_output[0]
+    
+    @pytest.fixture
+    def synthesis_intermediate_pyexps(self, synthesis_transform_output):
+        return synthesis_transform_output[1]
+    
+    def test_correctness(
+        self,
+        wavelet_index,
+        filter_params,
+        dwt_depth,
+        dwt_depth_ho,
+        quantisation_matrix,
+        analysis_input_linexp_array,
+        analysis_coeff_linexp_arrays,
+        synthesis_output_linexp_array,
+        synthesis_output_pyexp_array,
+    ):
+        # This test will simply attempt to maximise a real test signal and verify
+        # that the procedure appears to produce an improved result.
+        
+        max_quantisation_index = 63
+        
+        input_min = -512
+        input_max = 511
+        
+        # Make an arbitrary choice of target to maximise
+        synthesis_target_linexp_array = synthesis_output_linexp_array
+        synthesis_target_pyexp_array = synthesis_output_pyexp_array
+        
+        # Run against all filter phases as some phases may happen to be maximised
+        # by the test signal anyway
+        num_improved_phases = 0
+        for tx in range(synthesis_target_linexp_array.period[0]):
+            for ty in range(synthesis_target_linexp_array.period[1]):
+                # Produce test signal
+                test_signal, new_tx, new_ty, mx, my, tmx, tmy = make_synthesis_maximising_signal(
+                    analysis_input_linexp_array,
+                    analysis_coeff_linexp_arrays,
+                    synthesis_target_linexp_array,
+                    synthesis_output_linexp_array,
+                    tx, ty,
+                )
+                
+                synthesis_pyexp = synthesis_target_pyexp_array[new_tx, new_ty]
+                
+                kwargs = {
+                    "h_filter_params": filter_params,
+                    "v_filter_params": filter_params,
+                    "dwt_depth": dwt_depth,
+                    "dwt_depth_ho": dwt_depth_ho,
+                    "quantisation_matrix": quantisation_matrix,
+                    "synthesis_pyexp": synthesis_pyexp,
+                    "test_signal": test_signal,
+                    "input_min": input_min,
+                    "input_max": input_max,
+                    "max_quantisation_index": max_quantisation_index,
+                    "random_state": np.random.RandomState(1),
+                    "added_corruptions_per_iteration": (len(test_signal)+19)//20,  # 5%
+                    "removed_corruptions_per_iteration": (len(test_signal)+8)//5,  # 20%
+                    "added_iterations_per_improvement": 50,
+                    "terminate_early": None,
+                }
+                
+                # Run without any greedy searches to get 'baseline' figure
+                base_test_signal, base_decoded_value, base_qi, base_total_iterations = improve_synthesis_maximising_signal(
+                    number_of_searches=1,
+                    base_iterations=0,
+                    **kwargs
+                )
+                
+                # Run with greedy search to verify better result
+                imp_test_signal, imp_decoded_value, imp_qi, imp_total_iterations = improve_synthesis_maximising_signal(
+                    number_of_searches=3,
+                    base_iterations=100,
+                    **kwargs
+                )
+                assert imp_total_iterations >= 3 * 100
+                
+                # Ensure new test signal is normalised to polarities
+                for value in imp_test_signal.values():
+                    assert value in (-1, +1)
+                
+                # Should have improved over the test pattern alone
+                if abs(imp_decoded_value) > abs(base_decoded_value):
+                    num_improved_phases += 1
+                
+                # Check to see if decoded value matches what the pseudocode decoder
+                # would produce
+                xs, ys = zip(*imp_test_signal)
+                width = max(xs) + 1
+                height = max(ys) + 1
+                imp_test_signal_picture = [
+                    [
+                        input_min
+                        if imp_test_signal.get((x, y), 0) < 0 else
+                        input_max
+                        for x in range(width)
+                    ]
+                    for y in range(height)
                 ]
-                for y in range(height)
-            ]
-            kwargs = {
-                "width": width,
-                "height": height,
-                "wavelet_index": wavelet_index,
-                "wavelet_index_ho": wavelet_index,
-                "dwt_depth": dwt_depth,
-                "dwt_depth_ho": dwt_depth_ho,
-            }
-            actual_decoded_value = decode_with_vc2(
-                quantise_coeffs(
-                    encode_with_vc2(
-                        imp_test_signal_picture,
-                        **kwargs
+                kwargs = {
+                    "width": width,
+                    "height": height,
+                    "wavelet_index": wavelet_index,
+                    "wavelet_index_ho": wavelet_index,
+                    "dwt_depth": dwt_depth,
+                    "dwt_depth_ho": dwt_depth_ho,
+                }
+                actual_decoded_value = decode_with_vc2(
+                    quantise_coeffs(
+                        encode_with_vc2(
+                            imp_test_signal_picture,
+                            **kwargs
+                        ),
+                        imp_qi,
+                        quantisation_matrix,
                     ),
-                    imp_qi,
-                    quantisation_matrix,
-                ),
-                **kwargs
-            )[new_ty][new_tx]
-            
-            assert imp_decoded_value == actual_decoded_value
-            
-    # Consider the procedure to work if some of the phases are improved
-    assert num_improved_phases >= 1
+                    **kwargs
+                )[new_ty][new_tx]
+                
+                assert imp_decoded_value == actual_decoded_value
+                
+        # Consider the procedure to work if some of the phases are improved
+        assert num_improved_phases >= 1
+    
+    def test_terminate_early(
+        self,
+        synthesis_intermediate_pyexps,
+        filter_params,
+    ):
+        input_min = -512
+        input_max = 511
+        
+        test_signal = {(0, 0): -1}
+        
+        synthesis_pyexp = Argument("coeffs")[0]["LL"][0, 0]
+        
+        kwargs = {
+            "h_filter_params": filter_params,
+            "v_filter_params": filter_params,
+            "dwt_depth": 0,
+            "dwt_depth_ho": 0,
+            "quantisation_matrix": {0: {"LL": 0}},
+            "synthesis_pyexp": synthesis_pyexp,
+            "test_signal": test_signal,
+            "input_min": input_min,
+            "input_max": input_max,
+            "max_quantisation_index": 1,
+            "random_state": np.random.RandomState(1),
+            "base_iterations": 100,
+            "added_corruptions_per_iteration": 1,
+            "removed_corruptions_per_iteration": 0,
+            "added_iterations_per_improvement": 50,
+            "number_of_searches": 10,
+        }
+        
+        # Check that with terminate early disabled the search runs the full
+        # base set of iterations but finds no improvement
+        new_test_signal, new_decoded_value, new_qi, new_total_iterations = improve_synthesis_maximising_signal(
+            terminate_early=None,
+            **kwargs
+        )
+        assert new_test_signal == test_signal
+        assert new_total_iterations == 100 * 10
+        
+        # Check terminate early allows early termination after the specified
+        # number of iterations
+        new_test_signal, new_decoded_value, new_qi, new_total_iterations = improve_synthesis_maximising_signal(
+            terminate_early=3,
+            **kwargs
+        )
+        assert new_test_signal == test_signal
+        assert new_total_iterations == 100 * 3
+        
+        # Should run all iterations if an improvement is found
+        kwargs["test_signal"] = {(0, 0): +1}
+        new_test_signal, new_decoded_value, new_qi, new_total_iterations = improve_synthesis_maximising_signal(
+            terminate_early=3,
+            **kwargs
+        )
+        assert new_total_iterations >= 100 * 10
